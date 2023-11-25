23b8b85afd445badeba03d281b93ff4bfa5bd32b
ferdous <papiaferdous43@gmail.com>
2023-11-25 13:38:38 +0600

    add featured and footer section

--- src/pages/shared/Footer/Footer.jsx
+++ src/pages/shared/Footer/Footer.jsx
@@ -1,14 +1,24 @@
 
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
+
+
 const Footer = () => {
+
+    useEffect(() =>{
+        Aos.init();
+    },[])
+
     return (
         <div>
             <footer className="footer p-10 bg-gradient-to-b from-white to-90% to-pink-200 text-black">
                
-                    <aside >
+                    <aside data-aos="fade-left" data-aos-duration = "500">
                     <img className="h-[60px] w-[150px]" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSI5_FjG5s3MQ_--DpMZP2wYRhrpJeqRjvYYA&usqp=CAU" alt="" />
                         <p>ProShip Parcel Management Ltd.<br />Providing reliable tech since 1992</p>
                     </aside>
-                    <nav className="border-l-4 h-full border-red-500 pl-5">
+                    <nav className="border-l-4 h-full border-red-500 pl-5" data-aos="fade-up" data-aos-duration = "500">
                         <header className="footer-title ">Social</header>
                         <div className="grid grid-flow-col gap-4">
                             <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></a>
@@ -17,7 +27,7 @@ const Footer = () => {
                         </div>
                     </nav>
             </footer>
-            <div className="footer footer-center p-4 bg-slate-300 text-base-content">
+            <div className="footer footer-center p-4 bg-gradient-to-r from-purple-500 to-pink-500 text-black">
                 <p>Copyright © 2023 - All right reserved by ProShip Parcel Management Ltd</p>
                 </div>
         </div>
