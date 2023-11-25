ecd6d9f70ad51ad36286cd1467aa14e9f720780c
ferdous <papiaferdous43@gmail.com>
2023-11-25 13:11:03 +0600

    add navbar and banner section

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/Router.jsx'
+import {Helmet} from "react-helmet";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
      <div className='max-w-screen-xl mx-auto'>
+      <Helmet>
+        <title>ProShip Management</title>
+      </Helmet>
      <RouterProvider router={router} />
      </div>
   </React.StrictMode>,
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,9 +1,13 @@
+import { Helmet } from "react-helmet";
 import Features from "../Features/Features";
 import Header from "../Header/Header";
 
 const Home = () => {
     return (
         <div>
+            <Helmet>
+        <title>ProShip Parcel Mgmt. | Home</title>
+      </Helmet>
             <Header></Header>
             <Features></Features>
         </div>
